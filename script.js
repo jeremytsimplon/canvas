e86a9f097f9353897aa4859450293029d59d9875
JeremyTsimplon <jeremscorpion@gmail.com>
2015-12-15 11:35:37 +0100

    augmentation de la lineWidth du cercle

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var ctx = can.getContext("2d");
 
 ctx.beginPath();
 ctx.strokeStyle="rgb(52, 175, 236)" // couleur du cercle
-ctx.lineWidth = 20; // épaisseur du cercle
+ctx.lineWidth = 25; // épaisseur du cercle
 
 ctx.arc(200,200,140,0,Math.PI*2,true); // cercle de rayon 140px
 ctx.fillStyle="rgb(52, 175, 236)"; // couleur du premier carré
